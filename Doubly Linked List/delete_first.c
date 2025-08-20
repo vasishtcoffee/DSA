9edc20d61aad4cb5bfbc7e3112ddc4a26e8b0908
Vasisht K Gaddale <gitvasisht@gmail.com>
2025-08-20 08:28:52 +0530

    deleting fiirst node , modified

--- Doubly Linked List/delete_first.c
+++ Doubly Linked List/delete_first.c
@@ -7,9 +7,20 @@ struct node{
     struct node *prev;
 }*head;
 void delete_first(){
+
     struct node *temp=head;
+    if(head==NULL)
+    {
+        printf("Empty");
+        return;
+    }
+    if(head->next==NULL)
+    {
+        head=NULL;
+    }else{
     head=head->next;
     head->prev=NULL;
+    }
     free(temp);
     return;
 }
