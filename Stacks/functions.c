86dab2af5cc4678747193720f6c137414d2017c3
Vasisht K Gaddale <gitvasisht@gmail.com>
2025-08-30 17:29:13 +0530

    basic implementaion of stack functions

--- Stacks/functions.c
+++ Stacks/functions.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define INT_MIN  -1;
+#include<limits.h>
 struct Stack{
     int top;
     unsigned capacity;  //size of stack 
@@ -8,7 +8,7 @@ struct Stack{
 };
 struct Stack* create(unsigned capacity)
 {
-   struct Stack* stack=(struct stack*)malloc(sizeof(struct Stack));    //pointer to memory allocated using malloc
+   struct Stack* stack=(struct Stack*)malloc(sizeof(struct Stack));    //pointer to memory allocated using malloc
    stack->capacity=capacity;    //user allocates the capacity
    stack->top =-1;  // initially stack is empty 
    stack->array= (int*)malloc(stack->capacity *sizeof(int)); // creating array dynamically and creating space for capacity
@@ -28,12 +28,18 @@ void push(struct Stack* stack,int item){
     if(isFull(stack))
         return;  //return if stack is full
     stack->array[++stack->top]=item;  //first increment the top , so it will go above and insert the element
-    printf("%d is pushed",item);    //item is pushed
+    printf("%d is pushed\n",item);    //item is pushed
 }
 
 int pop(struct Stack* stack){
     if(isEmpty(stack))
-        return INT_MIN; // returns -1 if it is empty
+        return INT_MIN; // returns garbage value if it is empty
     return stack->array[stack->top--]; // just decrement the top pointer ,, the value at the actual top is not erased 
     //if push function is called again the value will be overidden
+}
+
+int peek(struct Stack* stack){
+    if(isEmpty(stack))
+        return INT_MIN; //return garbage value if it is empty
+    return stack->array[stack->top];
 }
